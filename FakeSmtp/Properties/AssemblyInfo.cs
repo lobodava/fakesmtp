35d772fda4c5fd783e62e6ca4fef960e9eb3db05
Vadim Loboda <lobodava@mail.ru>
2016-11-02 10:46:52 +0300

    Unused references removed

--- FakeSmtp/Properties/AssemblyInfo.cs
+++ FakeSmtp/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FakeSmtp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ASP.NET web interface for the fake smtp server")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Artezio.com")]
 [assembly: AssemblyProduct("FakeSmtp")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
